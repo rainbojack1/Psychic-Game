1d980db33f03795b7859d9ab72198b340ad23d45
Jesselyn Jackson <jesselyn.jackson@gm.com>
2019-06-13 10:49:50 -0400

    Initial file setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var randomLetter;
+var winCount = 0;
+var loseCount = 0;
+var guessCount = 0;
+var answers = [];
+
+document.getElementById("win").number = winCount;
+
+document.write("Can you see me on the screen?");
+console.log("Can you see me in the console?");
