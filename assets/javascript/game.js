52685d5d99b603c3f7f1df6b33ca1f7283e907d3
Jesselyn Jackson <jesselyn.jackson@gm.com>
2019-06-13 14:34:14 -0400

    Beginning functions - generate a random letter, compare it against user input, and initial loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,56 @@
 var randomLetter;
+var yourGuess = document.getElementById("yourGuess");
 var winCount = 0;
+var win = document.getElementById("win");
 var loseCount = 0;
-var guessCount = 0;
-var answers = [];
+var lose = document.getElementById("lose");
+var guessCount = 1097;
+var guess = document.getElementById("guess");
+var answerList = [];
+var answered = document.getElementById("answered");
+var letter;
 
-document.getElementById("win").number = winCount;
+win.textContent = winCount;
+lose.textContent = loseCount;
+guess.textContent = guessCount;
+answered.textContent = answerList;
 
-document.write("Can you see me on the screen?");
-console.log("Can you see me in the console?");
+function generateRandomLetter() {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(letter);
+    answerList.push(letter);
+    return letter;
+}
+
+function letsGo() {
+    for(var i = 0; i < 10; i++){
+        generateRandomLetter();
+        answerList.push(letter);
+    }
+
+    answered.textContent = answerList;
+    console.log(answerList);
+}
+
+function compare() {
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+        // Determines which key was pressed.
+        var userGuess = event.key;
+        //yourGuess.textContent = (userGuess + " vs " + letter);
+        console.log(userGuess + " vs " + letter);
+
+        if(letter === userGuess){
+            yourGuess.textContent = "You Win!";
+        }
+        else{
+            yourGuess.textContent = "You Lose, Try Again.";
+            generateRandomLetter();
+        }
+    }
+}
+
+generateRandomLetter();
+//letsGo();
+compare();
