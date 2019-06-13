6068e1a4bb95319f6c8e29b5a193fa7f951c17ee
Jesselyn Jackson <jesselyn.jackson@gm.com>
2019-06-13 16:04:08 -0400

    Modifying functions to fit the game rules. I still need to fix it so that the user can't continue guessing after 10 tries.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,42 @@
-var randomLetter;
 var yourGuess = document.getElementById("yourGuess");
 var winCount = 0;
 var win = document.getElementById("win");
 var loseCount = 0;
 var lose = document.getElementById("lose");
-var guessCount = 1097;
 var guess = document.getElementById("guess");
 var answerList = [];
 var answered = document.getElementById("answered");
 var letter;
+var i = 10;
 
 win.textContent = winCount;
 lose.textContent = loseCount;
-guess.textContent = guessCount;
+guess.textContent = i;
 answered.textContent = answerList;
+
+document.write(39);
 
 function generateRandomLetter() {
     var alphabet = "abcdefghijklmnopqrstuvwxyz";
     letter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(letter);
-    answerList.push(letter);
+    console.log("letter: ", letter);
+
     return letter;
 }
 
 function letsGo() {
-    for(var i = 0; i < 10; i++){
-        generateRandomLetter();
-        answerList.push(letter);
+    //Get a random letter
+    generateRandomLetter();
+    if(i > 0){
+        compare();
+
+        win.textContent = winCount;
+        lose.textContent = loseCount;
+        guess.textContent = i;
+        answered.textContent = answerList;
     }
 
-    answered.textContent = answerList;
-    console.log(answerList);
+
 }
 
 function compare() {
@@ -39,18 +45,37 @@ function compare() {
         // Determines which key was pressed.
         var userGuess = event.key;
         //yourGuess.textContent = (userGuess + " vs " + letter);
+        answerList.push(userGuess);
         console.log(userGuess + " vs " + letter);
 
-        if(letter === userGuess){
-            yourGuess.textContent = "You Win!";
-        }
-        else{
-            yourGuess.textContent = "You Lose, Try Again.";
-            generateRandomLetter();
-        }
+            if(letter === userGuess){
+                winCount++;
+                yourGuess.textContent = userGuess;
+                i--;
+                alert("You Win!");
+            }
+            else{
+                loseCount++;
+                yourGuess.textContent = userGuess;
+                i--;
+                alert("You Lose, Try Again.");
+            }
+
+        if(i < 0){
+                alert("Game Over!");
+            }
+
+        generateRandomLetter();
+        win.textContent = winCount;
+        lose.textContent = loseCount;
+        guess.textContent = i;
+        answered.textContent = answerList;
     }
+
+    //letsGo();     THIS CAUSES AN ENDLESS LOOP!
+
 }
 
-generateRandomLetter();
-//letsGo();
-compare();
+//generateRandomLetter();
+letsGo();
+//compare();
